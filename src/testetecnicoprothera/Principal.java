8de13ca69f6d3da61fa9b8d9aac67d85d98643dd
clararicioni <bergamoclara9@gmail.com>
2026-09-17 13:40:39 -0300

    inicializando projeto e classes

---
+++ src/testetecnicoprothera/Principal.java
@@ -0,0 +1,12 @@
+package testetecnicoprothera;
+/**
+ *
+ * @author Clara Bergamo
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
